dc717abc909b7ee86bfc9fa85d516f0085eb6e2b
Matt Greig <mattg.vfx@gmail.com>
2018-09-04 22:53:32 +1000

    updated notebook to display plot and renamed profiler class

---
+++ profiler.py
@@ -0,0 +1,33 @@
+import json
+
+
+class Profile(object):
+
+    def __init__(self, profile_dict):
+        self._name = next(iter(profile_dict))
+        self._curves = profile_dict[self._name]
+        self._samples = len(self._curves)
+
+        # print self._name
+        # print self._curves
+        # print self._samples
+
+    def getCurveForChannel(self, channel='r'):
+        """ returns an ordered list of samples @ time """
+        result = []
+        for sample in range (1, self._samples + 1):
+            result.append(self._curves[str(sample)][channel])
+        return result
+
+
+def get_json(path):
+    """ retrieves json file from disk and saves it as a dict """
+
+    f = open(path, 'r')
+    profiles_dict = json.load(f)
+
+    return profiles_dict
+
+data = get_json('data/ocio_aces.json')
+
+
